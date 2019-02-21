cd83d5a5bcb1cb4a2d8de3d76351831b2895c86b
blake-swl <seulwoolee93@gmail.com>
2019-02-20 16:46:39 -0800

    [add] right screen layout

--- public/src/Components/App/index.jsx
+++ public/src/Components/App/index.jsx
@@ -8,7 +8,7 @@ const App = () => (
   <div>
     <Left />
     <Center />
-    {/* <Right /> */}
+    <Right />
   </div>
 );
 
--- public/src/Components/RightScreen/index.jsx
+++ public/src/Components/RightScreen/index.jsx
@@ -3,7 +3,42 @@ import './right.scss';
 
 const Right = () => (
   <div id="right">
-    <div></div>
+    <div className="rightHeader">
+      <h1>Drinks & Snacks on the Go</h1>
+    </div>
+    <div className="snackItems">
+      <h2>Premade Rolls</h2>
+      <div className="item">
+        <div className="itemNprice">
+          <h3>Cup Ramen</h3>
+          <h3>$$$</h3>
+        </div>
+          {/* <p>Hawaiian Salmon, Hawaiian Tuna, Spicy Tuna, Crabmeat, Spicy Mayo, Eel Sauce</p> */}
+        <div className="itemNprice">
+          <h3>California Roll</h3>
+          <h3>$$$</h3>
+        </div>
+        <div className="itemNprice">
+          <h3>Spicy Tuna Roll</h3>
+          <h3>$$$</h3>
+        </div>
+        <div className="itemNprice">
+          <h3>Salmon Roll</h3>
+          <h3>$$$</h3>
+        </div>
+      </div>
+      <h2>Drinks</h2>
+      <div className="item">
+        <div className="itemNprice">
+          <h3>Can soda</h3>
+          <h3>$$$</h3>
+        </div>
+        <div className="itemNprice">
+          <h3>Thai Tea</h3>
+          <h3>$$$</h3>
+        </div>
+      </div>
+    </div>
   </div>
 );
 export default Right;
